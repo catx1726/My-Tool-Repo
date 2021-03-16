967d233bacdccbe6a4faa8c8f10bc705a6da4a76
catx1726 <catx1726@foxmail.com>
2021-03-16 21:33:00 +0800

    update: lazyload

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,7 +4,7 @@ module.exports = {
   // 末尾不需要逗号,注意与 eslint 的冲突
   trailingComma: 'none',
   // 一行最多 100 字符,与eslint 有冲突
-  printWidth: 100,
+  printWidth: 120,
   // 使用 2 个空格缩进
   tabWidth: 2,
   // 不使用缩进符，而使用空格
