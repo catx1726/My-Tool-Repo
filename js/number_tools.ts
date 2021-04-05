a89220c3dcb28a3bdea6a3a42d614695dc7a2e4a
cad <catx1726@foxmail.com>
2021-04-05 09:20:16 +0800

    update: 2021年4月5日

--- js/number_tools.ts
+++ js/number_tools.ts
@@ -7,11 +7,11 @@ console.log(num1, num2, num3)
 // 精确保留小数位
 // DES 1.597 会有BUG，进位后结果为 1.6，所以最好不要四舍五入，如果要四舍五入的话要考虑补0问题
 // let fn = (num, decimal) => Math.round(num * 10 ** decimal) / 10 ** decimal
-let fn = (num, decimal) => Math.floor(num * 10 ** decimal) / 10 ** decimal
-let num = fn(1.597, '2')
+let fn = <T extends number>(num: T, decimal: T) => Math.floor(num * 10 ** decimal) / 10 ** decimal
+let num = fn(1.597, 2)
 console.log(num)
 
-// 范围值
-let random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+// 范围值[1,10]
+let random = <T extends number>(min: T, max: T) => Math.floor(Math.random() * (max - min + 1)) + min
 let num4 = random(1, 10)
 console.log(num4)
